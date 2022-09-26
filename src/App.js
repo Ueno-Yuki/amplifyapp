900ea1fa0cd03fce6010d545110310283e3d0271
yuki_ueno <yuki_ueno@xonor.jp>
2022-09-26 15:34:29 +0900

    Operation check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import routes from "./routes.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      {routes.map((route, index) => (
+        <Route
+          path={route.path}
+          exact={route.exact}
+          component={route.component}
+          key={index}
+        />
+      ))}
+    </Switch>
   );
 }
 
-export default App;
+export default withRouter(App);
